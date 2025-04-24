eb4d442de0bd278e5b8e3e6dcb4dae3c004f3858
Михаил <shonovgacha@gmail.com>
2025-04-24 21:43:47 +0300

    УРАААААААААА ПОЛУЧИЛОСЬ!2

--- src/api/data.js
+++ src/api/data.js
@@ -1,5 +1,12 @@
 import img1 from '../img/shark.png'
 import img2 from '../img/giraffe.png'
+import img3 from '../img/bear.png'
+import img4 from '../img/петух.png'
+import img5 from '../img/мопс.png'
+import img6 from '../img/зайка.png'
+import img7 from '../img/белочка.png'
+import img8 from '../img/енотик.png'
+import img9 from '../img/динозавр.png'
 
 export const json = `{
     "catalogs": [
@@ -26,7 +33,92 @@ export const json = `{
            "img": "${img2}",
             "width": "20 см",
             "height": "35 см"
+        },
+        {
+            "id": "3",
+            "type": "Мягкая игрушка",
+            "name": "Медвежонок",
+            "startPrice": 0,
+            "price": "470",
+            "count": "1",
+            "show": false,
+           "img": "${img3}",
+            "width": "30 см",
+            "height": "45 см"
+        },
+        {
+            "id": "4",
+            "type": "Мягкая игрушка",
+            "name": "Петушок",
+            "startPrice": 0,
+            "price": "360",
+            "count": "1",
+            "show": false,
+           "img": "${img4}",
+            "width": "20 см",
+            "height": "35 см"
+        },
+        {
+            "id": "5",
+            "type": "Мягкая игрушка",
+            "name": "Мопс",
+            "startPrice": 0,
+            "price": "360",
+            "count": "1",
+            "show": false,
+           "img": "${img5}",
+            "width": "20 см",
+            "height": "35 см"
+        },
+        {
+            "id": "6",
+            "type": "Мягкая игрушка",
+            "name": "Зайка",
+            "startPrice": 0,
+            "price": "410",
+            "count": "1",
+            "show": false,
+           "img": "${img6}",
+            "width": "20 см",
+            "height": "35 см"
+        },
+        {
+            "id": "7",
+            "type": "Мягкая игрушка",
+            "name": "Белочка",
+            "startPrice": 0,
+            "price": "410",
+            "count": "1",
+            "show": false,
+           "img": "${img7}",
+            "width": "20 см",
+            "height": "35 см"
+        },
+        {
+            "id": "8",
+            "type": "Мягкая игрушка",
+            "name": "Енотик",
+            "startPrice": 0,
+            "price": "410",
+            "count": "1",
+            "show": false,
+           "img": "${img8}",
+            "width": "20 см",
+            "height": "35 см"
+        },
+        {
+            "id": "9",
+            "type": "Мягкая игрушка",
+            "name": "Динозавр",
+            "startPrice": 0,
+            "price": "500",
+            "count": "1",
+            "show": false,
+           "img": "${img9}",
+            "width": "20 см",
+            "height": "35 см"
         }
+            
     ]
 }
 `
